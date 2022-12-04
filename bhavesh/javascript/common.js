32938f591adf0b42b2b8ebd5e3b9f834ed60be80
Bhavesh Sooka <bsooka@gmail.com>
2022-12-04 21:15:45 +0200

    code cleanup

--- bhavesh/javascript/common.js
+++ bhavesh/javascript/common.js
@@ -14,39 +14,3 @@ export function getQuestionData(url) {
     },
   });
 }
-
-export function sumAndMax(array, delimiter) {
-  let sum = 0;
-  let max = -1;
-  array.forEach((item) => {
-    if (item === delimiter) {
-      if (sum > max) {
-        max = sum;
-      }
-      sum = 0;
-    } else {
-      let calories = Number.parseInt(item);
-      sum += calories;
-    }
-  });
-
-  return max;
-}
-
-export function sumTopThree(array, delimiter) {
-  let sum = 0;
-  let maxes = [];
-  array.forEach((item) => {
-    if (item === delimiter) {
-      maxes.push(sum);
-      sum = 0;
-    } else {
-      let calories = Number.parseInt(item);
-      sum += calories;
-    }
-  });
-
-  let sorted = maxes.sort((a, b) => b - a);
-
-  return sorted[0] + sorted[1] + sorted[2];
-}
