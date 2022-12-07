865bc50e06853da59fd6e8615747d4f372431979
Bhavesh Sooka <bsooka@gmail.com>
2022-12-07 10:51:45 +0200

    day7

--- bhavesh/javascript/index.js
+++ bhavesh/javascript/index.js
@@ -6,6 +6,7 @@ import { day3 } from "./aoc-2022/day-3-rucksack-reorganization.js";
 import { day4 } from "./aoc-2022/day-4-camp-cleanup.js";
 import { day5 } from "./aoc-2022/day-5-supply-stacks.js";
 import { day6 } from "./aoc-2022/day-6-tuning-trouble.js";
+import { day7 } from "./aoc-2022/day-7-no-space-left-on-device.js";
 
 let answers = [];
 
@@ -15,5 +16,6 @@ await day3().then((answer) => answers.push(answer));
 await day4().then((answer) => answers.push(answer));
 await day5().then((answer) => answers.push(answer));
 await day6().then((answer) => answers.push(answer));
+await day7().then((answer) => answers.push(answer));
 
 console.log(answers);
